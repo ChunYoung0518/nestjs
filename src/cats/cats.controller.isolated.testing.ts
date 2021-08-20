beb8347ccc0defa1a375026f3cc70634ec892566
chunyoung0518@gmail.com <chunyoung0518@gmail.com>
2021-08-20 15:28:32 +1000

    unit testing

---
+++ src/cats/cats.controller.isolated.testing.ts
@@ -0,0 +1,21 @@
+import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
+
+describe('CatsController', () => {
+  let catsController: CatsController;
+  let catsService: CatsService;
+
+  beforeEach(() => {
+    catsService = new CatsService();
+    catsController = new CatsController(catsService);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = [{ name: 'kitty', age: 5, breed: 'Australia cat' }];
+      jest.spyOn(catsService, 'findAll').mockImplementation(() => result);
+
+      expect(await catsController.findAll()).toBe(result);
+    });
+  });
+});
--- src/cats/cats.controller.spec.ts
+++ src/cats/cats.controller.spec.ts
@@ -4,6 +4,7 @@ import { CatsService } from './cats.service';
 
 describe('CatsController', () => {
   let controller: CatsController;
+  let service: CatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +12,22 @@ describe('CatsController', () => {
       providers: [CatsService],
     }).compile();
 
+    // resolve method used to dynamically resolve scoped providers (transient or request-scoped)
+    // service = await .module.resolve(CatsService);
+    service = module.get<CatsService>(CatsService);
     controller = module.get<CatsController>(CatsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = [{ name: 'kitty', age: 5, breed: 'Australia cat' }];
+      jest.spyOn(service, 'findAll').mockImplementation(() => result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
   });
 });
